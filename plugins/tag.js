c77cd86df3bcc25e5312ba4e6607c3b89c38ad82
unknownalien28 <109808373+unknownalien28@users.noreply.github.com>
2022-08-24 15:29:07 +0100

    Update tag.js

--- plugins/tag.js
+++ plugins/tag.js
@@ -9,7 +9,7 @@ const {readFileSync} = require('fs');
 const {saveMessage} = require('./misc/saveMessage');
 const Lang = getString('group');
 Module({pattern: 'tag|hidetag ?(.*)',use: 'group', fromMe: true, desc: Lang.TAGALL_DESC}, (async (message, match) => {
-if (!message.jid.endsWith('@g.us')) return await message.sendMessage(Lang.GROUP_COMMAND)
+if (!message.jid.endsWith('@g.us')) return await message.send(Lang.GROUP_COMMAND)
 if (match[1] === "all" || (match[1] && match[1].startsWith("ad")) || !message.reply_message) return;
 var target = message.jid
 if (match[1] && match[1].endsWith("us") && /[0-9]+(-[0-9]+|)(@g.us|@s.whatsapp.net)/g.test(match[1])) target = [...match[1].match(/[0-9]+(-[0-9]+|)(@g.us|@s.whatsapp.net)/g)];
@@ -20,7 +20,7 @@ jids.push(user.id.replace('c.us', 's.whatsapp.net'));});
 await message.forwardMessage(target,message.quoted,{detectLinks: true,contextInfo: {mentionedJid: jids}});
 }))
 Module({pattern: 'tagadmin',use: 'group', fromMe: true, desc: "Tags admins",usage: '.tag or .tag jid'}, (async (message, match) => {
-    if (!message.jid.endsWith('@g.us')) return await message.sendMessage(Lang.GROUP_COMMAND)
+    if (!message.jid.endsWith('@g.us')) return await message.send(Lang.GROUP_COMMAND)
     if (!message.reply_message) return;
     var group = await message.client.groupMetadata(message.jid)
     var jids = [];
@@ -31,14 +31,14 @@ Module({pattern: 'tagadmin',use: 'group', fromMe: true, desc: "Tags admins",usag
 }))
 Module({pattern: 'forward ?(.*)',use: 'utility', fromMe: true, desc: "Forwards message"}, (async (message, match) => {
     if (!message.reply_message) return await message.sendReply("*Reply to a message*\n*Ex: .forward jid jid ...*")
-    let Jids = [...match[1].match(/[0-9]+(-[0-9]+|)(@g.us|@s.whatsapp.net)/g)] || [message.jid]
+    let Jids = [...match[1]?.match(/[0-9]+(-[0-9]+|)(@g.us|@s.whatsapp.net)/g)] || [message.jid]
         for (let jid of Jids) {
       await message.forwardMessage(jid, message.quoted,{detectLinks: true});
     }
 }));
 Module({pattern: 'send ?(.*)',use: 'utility', fromMe: true, desc: "Sends message"}, (async (message, match) => {
     if (!message.reply_message) return await message.sendReply("*Reply to a message*\n*Ex: .send jid jid ...*")
-    let Jids = [...match[1].match(/[0-9]+(-[0-9]+|)(@g.us|@s.whatsapp.net)/g)] || [message.jid];
+    let Jids = [...match[1]?.match(/[0-9]+(-[0-9]+|)(@g.us|@s.whatsapp.net)/g)] || [message.jid];
         for (let jid of Jids) {
       await message.forwardMessage(jid, message.quoted,{contextInfo:{isForwarded: false},detectLinks: true});
     }
